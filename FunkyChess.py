4e5847fa413472c6e44f3a74c207aac6a57f1d0f
Santiago Benoit <santib27@gmail.com>
2024-02-21 23:16:35 -0500

    description, better randomness

--- FunkyChess.py
+++ FunkyChess.py
@@ -1,6 +1,32 @@
+# Funky Chess Variant AKA Funky Freestyle Chess
+# By Santiago Benoit
+# 
+# Purpose:
+# - Create games that are diverse, unique, and interesting
+# - No theory (practically speaking), thinking starts at the beginning
+# - Likely asymmetric
+# - No unfair advantage, balanced for both players
+# - Gives both players many options for how to play
+# - Can be played with standard chess set
+# 
+# Rules:
+# - Set up boards like in "transcendental chess":
+# -- Shuffle both backranks, asymmetry allowed, bishops on opposite colors
+# - No castling rights
+# - To deal with imbalance:
+# -- Engine makes the move with evaluation closest to equal (or custom odds)
+# -- All legal moves considered, ties broken randomly
+# -- Engine continues making moves until evaluation is within tolerance
+# -- At least 1 move will be made
+# -- Players start at resulting position (including turn, en passant, half and fullmove clocks)
+# - Gameplay can possibly start with either white or black, pieces off the board, or player in check!
+# - Aside from the starting position, same rules as regular chess
+
+
 import argparse
 import chess
 import numpy as np
+import secrets
 from stockfish import Stockfish
 
 
@@ -14,10 +40,6 @@ def balance(fen, args):
         legal = list(board.legal_moves)
         moves = engine.get_top_moves(len(legal))
         moves = [(move["Move"], move["Centipawn"] / 100) for move in moves if move["Centipawn"] is not None]
-        best, val = moves[0]
-        error = abs(val - args.odds)
-        if error <= args.tolerance:
-            break
         np.random.shuffle(moves)
         move, val = min(moves, key=lambda x: abs(x[1] - args.odds))
         error = abs(val - args.odds)
@@ -46,9 +68,9 @@ def opposite_color_bishops(row):
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--engine", type=str, default="./stockfish", help="path to Stockfish engine binary (default ./stockfish)")
-    parser.add_argument("--depth", type=int, default=30, help="evaluation depth (default 30)")
-    parser.add_argument("--threads", type=int, default=7, help="engine CPU threads (default 7)")
-    parser.add_argument("--hash", type=int, default=8192, help="engine hash size (default 8192)")
+    parser.add_argument("--depth", type=int, default=20, help="evaluation depth (default 20)")
+    parser.add_argument("--threads", type=int, default=1, help="engine CPU threads (default 1)")
+    parser.add_argument("--hash", type=int, default=1024, help="engine hash size (default 1024)")
     parser.add_argument("--seed", type=int, default=None, help="random seed (default random)")
     parser.add_argument("--odds", type=float, default=0.0, help="target evaluation (default 0.0)")
     parser.add_argument("--tolerance", type=float, default=0.1, help="imbalance tolerance (default 0.1)")
@@ -73,9 +95,11 @@ def main():
     del engine
     seed = args.seed
     if seed is None:
-        seed = np.random.randint(1 << 32)
-    print("Seed:", seed)
+        seed = secrets.randbits(32)
+    print("Random seed:", seed)
     np.random.seed(seed)
+    if args.threads > 1:
+        print("Warning: random seed consistency is not guaranteed with threads > 1.")
     print("\nGenerating position...\n")
     white = ["R", "N", "B", "Q", "K", "B", "N", "R"]
     black = ["r", "n", "b", "q", "k", "b", "n", "r"]
